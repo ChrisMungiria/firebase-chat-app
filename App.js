887f1e819342f5d72a0879ff97736475ed78643f
Chris Mungiria <christopher.mungiria@strathmore.edu>
2024-04-14 20:15:26 +0300

    feat: added react redux and navigation

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Navigation imports
+import "react-native-gesture-handler";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
+// Auth Stack
+import Login from "./screens/auth/login";
+import Register from "./screens/auth/register";
+
+// Protected Stack
+import Home from "./screens/protected/Home";
+
+// React Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
+function ProtectedStack() {
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function AuthStack() {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Register" component={Register} />
+    </Stack.Navigator>
+  );
+}
+
+function Main() {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <AuthStack />
+      </NavigationContainer>
+    </Provider>
+  );
+}
+
+export default function App() {
+  return <Main />;
+}
---
+++ screens/protected/Home.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <View className="w-screen h-screen flex items-center justify-center">
+      <Text className="text-red-500">Home</Text>
+    </View>
+  );
+};
+
+export default Home;
